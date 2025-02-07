c4701baa02baca59b61b56d31abb3c9b046f0c53
lewisombaka <lewisochieng99@gmail.com>
2025-02-07 13:12:47 +0300

    start

--- week2/main.java
+++ week2/main.java
@@ -4,5 +4,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println(5+"5");
         System.out.println("Main Repo");
+        System.out.println("Trial");
     }
 }
