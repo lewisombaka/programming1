38bc56deeaaee27a7b6c84b7737feff54dd98c03
lewisombaka <lewisochieng99@gmail.com>
2025-02-07 17:47:32 +0300

    while and do while

--- week2/lg2.java
+++ week2/lg2.java
@@ -5,15 +5,30 @@ public class lg2 {
         // exception handling
         // int myInt = Integer.parseInt("s");
         // System.out.println(myInt);
-        try {
-            int myInt = Integer.parseInt("5");
-            System.out.println(myInt);
+        // try {
+        //     int myInt = Integer.parseInt("5");
+        //     System.out.println(myInt);
+        // }
+        // catch(NumberFormatException nfe){
+        //     System.out.println("Enter a number");
+        // }
+        // finally{ // finally statement always runs
+        //     System.out.println("Good");
+        // }
+
+        // Do while loop
+        int i = 1;
+        System.out.println("do While");
+        do {
+            System.out.println("Hello" + i);
+            i++;
         }
-        catch(NumberFormatException nfe){
-            System.out.println("Enter a number");
-        }
-        finally{ // finally statement always runs
-            System.out.println("Good");
+        // int j = 1;
+        while (i<=3);
+        int j = 1;
+        while (j<=3){
+            System.out.println("Hello"+j);
+            j++;
         }
 
     }
