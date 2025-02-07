c9b3c873e2a6555a00090c69293a1721f6079f87
lewisombaka <lewisochieng99@gmail.com>
2025-02-07 08:44:48 +0300

    start

--- week2/main.java
+++ week2/main.java
@@ -3,5 +3,6 @@ package week2;  // Add this if your file is inside the "week2" folder
 public class main {
     public static void main(String[] args) {
         System.out.println(5+"5");
+        System.out.println("Main Repo");
     }
 }
