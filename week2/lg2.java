05acf6080e191ba04d217c72c751d95c3d45fe16
lewisombaka <lewisochieng99@gmail.com>
2025-02-07 13:30:44 +0300

    exception handling

---
+++ week2/lg2.java
@@ -0,0 +1,9 @@
+package week2;
+
+public class lg2 {
+    public static void main(String[] args) {
+        // exception handling
+        int myInt = Integer.parseInt("5");
+        System.out.println(myInt);
+    }
+}
