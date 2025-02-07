73c7a67b4450e65dee4c103851f803f68a92d26a
lewisombaka <lewisochieng99@gmail.com>
2025-02-07 17:18:35 +0300

    finally

--- week2/lg2.java
+++ week2/lg2.java
@@ -3,7 +3,18 @@ package week2;
 public class lg2 {
     public static void main(String[] args) {
         // exception handling
-        int myInt = Integer.parseInt("5");
-        System.out.println(myInt);
+        // int myInt = Integer.parseInt("s");
+        // System.out.println(myInt);
+        try {
+            int myInt = Integer.parseInt("5");
+            System.out.println(myInt);
+        }
+        catch(NumberFormatException nfe){
+            System.out.println("Enter a number");
+        }
+        finally{ // finally statement always runs
+            System.out.println("Good");
+        }
+
     }
 }
