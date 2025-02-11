96b8df3c9bab3d95234d9470ae4abd291f054668
lewisombaka <lewisochieng99@gmail.com>
2025-02-11 05:28:44 +0300

    do-while loop

--- week2/discussionforum.java
+++ week2/discussionforum.java
@@ -4,11 +4,21 @@ import java.util.Scanner;
 public class discussionforum {
     public static void main (String [] args){
         Scanner scanner = new Scanner (System.in);
+        
+        //while loop
         String input = "";
         while (!input .equals("exit")) {
             System.out.println("Enter input ");
             input = scanner.nextLine();
             System.out.println("You entered: " + input);
         }
+
+        // do-while loop
+        int choice;
+        do {
+            System.out.println("Enter your guess between 0 to 9");
+            choice = scanner.nextInt();
+        }
+        while (choice != 2);
     }
 }
