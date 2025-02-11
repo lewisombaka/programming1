1010394d4ca381a4d7db25182077a04d5a2ae1b2
lewisombaka <lewisochieng99@gmail.com>
2025-02-11 06:01:08 +0300

    if else

--- week2/discussionforum.java
+++ week2/discussionforum.java
@@ -6,24 +6,39 @@ public class discussionforum {
         Scanner scanner = new Scanner (System.in);
         
         //while loop
-        String input = "";
-        while (!input .equals("exit")) {
-            System.out.println("Enter input ");
-            input = scanner.nextLine();
-            System.out.println("You entered: " + input);
-        }
+        // String input = "";
+        // while (!input .equals("exit")) {
+        //     System.out.println("Enter input ");
+        //     input = scanner.nextLine();
+        //     System.out.println("You entered: " + input);
+        // }
 
         // do-while loop
-        int choice;
-        do {
-            System.out.println("Enter any Number to start and 0 to exit");
-            choice = scanner.nextInt();
-        }
-        while (choice != 0);
+        // int choice;
+        // do {
+        //     System.out.println("Enter any Number to start and 0 to exit");
+        //     choice = scanner.nextInt();
+        // }
+        // while (choice != 0);
 
         // for loop
         for (int i = 1; i <= 5; i++) {
             System.out.println("Iteration "+i);
         }
+
+        System.out.println("Enter Score ");
+        int score = scanner.nextInt();
+        String grade = "";
+        if (score >= 90){
+            grade = "A";
+        }
+        else if (score >= 80) {
+            grade = "B";
+        }
+        else {
+            grade = "C";
+        }
+
+        System.out.println("Your grade is "+ grade);
     }
 }
