64e1dfce8f7504cf2a86e9d963a0843610f07c5a
lewisombaka <lewisochieng99@gmail.com>
2025-02-12 06:46:19 +0300

    library sytem

---
+++ week2/programmingassignment.java
@@ -0,0 +1,8 @@
+package week2;
+import java.util.Scanner;
+
+public class programmingassignment {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
