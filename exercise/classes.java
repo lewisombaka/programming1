cc673c1b30408bf3919765f795c8818981d10ee9
lewisombaka <lewisochieng99@gmail.com>
2025-02-24 07:01:34 +0300

    add classes

--- exercise/classes.java
+++ exercise/classes.java
@@ -5,14 +5,17 @@ public class classes {
         // classes and objects
         // class has instances(str and int) and methods
         // class should be single entity - if it's a class that contains people then it should not contain animals
-        pokemon p1 = new pokemon();
-        p1.level = 23;
-        p1.name = "Pikachu";
+        pokemon p1 = new pokemon("Pikachu", 23);
+        // p1.level = 23;
+        // p1.name = "Pikachu";
 
         System.out.println(p1.name+" "+p1.level);
 
-        pokemon p2 = new pokemon();
-        p2.name = "eeevu";
+        pokemon p2 = new pokemon("eeevu", 12);
+        // p2.name = "eeevu";
         p2.attack();
+
+        pokemon p3 = new pokemon("Lewis", 25);
+        System.out.println(p3.level);
     }
 }
--- exercise/pokemon.java
+++ exercise/pokemon.java
@@ -4,6 +4,15 @@ public class pokemon {
     String name;
     int level;
 
+    pokemon (){
+        level = 1;
+    }
+
+    pokemon(String pName, int pLevel){
+        name = pName;
+        level = pLevel;
+    }
+
     void attack(){
         System.out.println(name+ " attack!");
     }
