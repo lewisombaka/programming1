82e9ba16ef377f2d3f90cd410481fc6bdbd23214
lewisombaka <lewisochieng99@gmail.com>
2025-03-04 18:03:40 +0300

    single-dimensional array

---
+++ exercise/codingwithjohn/arrays.java
@@ -0,0 +1,11 @@
+package exercise.codingwithjohn;
+
+public class arrays {
+    public static void main(String[] args) {
+        // ArraysVsArrayList
+        // single-dimensional arrays in java
+        // Null - value of an object that references nothing String text;
+        //Arrays is a collection if variables of the same data type, array size is fixed
+
+    }
+}
