b15fb591aa351e3b5acdac85a9a7c709e6157daf
lewisombaka <lewisochieng99@gmail.com>
2025-03-13 12:47:50 +0300

    koc

--- week05/oop.java
+++ week05/oop.java
@@ -1,5 +1,33 @@
 package week05;
 
+// import exercise.pokemon;
+
+class Classname {
+    // instance variables
+    int var1;
+    String var2;
+
+    // methods
+    public void method1(/*parameters*/){
+        //body of methods
+    }
+}
+
+class Pokemon {
+    String name;
+    int level;
+    int age;
+
+    public void attack (){
+        System.out.println(name + " Attack!");
+    }
+    // constructors
+    // have the same name as the class name and they are default
+    Pokemon(){
+        age = 10;
+    }
+}
+
 public class oop {
     public static void main(String[] args) {
         user u = new user();
@@ -27,6 +55,20 @@ public class oop {
         vehicle.start(); // instance of cars class // instance methods
 
         // classes and objects
+        // class serves as a blue print of creating an object 
+        // // object serves as an instance of a class
+        // classes should be single entities
+
+        Pokemon poki = new Pokemon(); // poki object created
+        poki.name = "Pikachu";
+        poki.level = 3; // using the dot notations
+
+        System.out.println(poki.name + poki.level);
+        System.out.println(poki.age);
+
+        // constructors
+
+        
     }
 
 }
